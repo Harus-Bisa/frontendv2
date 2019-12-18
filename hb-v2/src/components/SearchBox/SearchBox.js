ed3eb43d93112ad9279695550e22e4671ed0ef20
Danika Widjaja <danika.dew@utexas.edu>
2019-12-18 14:57:24 -0600

    search box updated functionality

--- hb-v2/src/components/SearchBox/SearchBox.js
+++ hb-v2/src/components/SearchBox/SearchBox.js
@@ -1,24 +1,96 @@
 import React from "react";
-import { Button, Input } from "reactstrap";
+import { Input } from "reactstrap";
 import PropTypes from 'prop-types';
 import { withRouter } from "react-router-dom";
+import {Autocomplete} from '@material-ui/lab'
+import { TextField } from "@material-ui/core";
 
+const profs = [
+    {name:'Agus Hartanto', school:'Universitas Atma Jaya', id:'1'},
+    {name:'Agus C. Kristiono', school:'Universitas Maranata', id:'2'},
+    {name:'Agus W. Soehadi', school:'Universitas Prasetya Mulya', id:'3'}
+]
 function SearchBox(props){
     var [text, setText] = React.useState("")
-    const search = () =>{
-        props.search();
-        props.history.push("/review/1")
+    var [open, setOpen] = React.useState(false)
+    var [options, setOptions] = React.useState([])
+    const loading = open && options.length === 0
+
+    React.useEffect(() => {
+        let active = true;
+        
+        if (!loading){
+            return undefined;
+        }
+
+        const search = (async () => {
+            const response = props.search(text);
+            if(active){
+                setOptions(response)
+            }
+        });
+        
+        if(text.length > 0){
+            search();
+        }
+        else{
+            setOptions([])
+        }
+        return () =>{
+            active = false
+        }
+    }, [loading, text])
+
+    React.useEffect(() => {
+        if(!open){
+            setOptions([])
+            setText("")
+        }
+    }, [open]);
+
+    React.useEffect(() => {
+        if (text.length === 0){
+            setOptions([])
+        }
+    }, [text])
+
+    const select = (id) =>{
+        props.history.push("/review/"+id)
     }
     return(
         <div className="search-box">
-            <Input type='text' id="search-box" placeholder="Cari nama Dosen anda" value={text} onChange={(event) => setText(event.target.value) }/>
-            <Button className="green-button" onClick={search}>Cari</Button>
+            <Autocomplete
+                id="search-box"
+                style={{width:'100%'}}
+                open={open}
+                onOpen={() => {setOpen(true)}}
+                onClose={() => setOpen(false)}
+                options={options}
+                loading={loading}
+                noOptionsText="Dosen tidak ditemukan."
+                renderInput={params => (
+                    <TextField
+                      {...params}
+                      placeholder="Cari nama dosen Anda"
+                      fullWidth
+                      onChange={(event) => setText(event.target.value)}
+                    />
+                )}
+                renderOption={option => {
+                    return(
+                        <div onClick={() => select(option.id)} style={{width:'100%'}}>
+                            <p style={{fontWeight:'bold', margin:0}}>{option.name}</p>
+                            <p style={{fontSize:'12px', margin:0}}>{option.school}</p>    
+                        </div>
+                    )
+                }}
+            />
         </div>
     )
 }
 
 SearchBox.defaultProps={
-    search: () => {}
+    search: (text) => {return profs}
 }
 
 SearchBox.propTypes={
