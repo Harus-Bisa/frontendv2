e67246f0823f102a79ddff10390e9adc0e0ea124
Danika Widjaja <danika.dew@utexas.edu>
2019-12-23 17:25:41 -0600

    update margin

--- hb-v2/src/components/Card/ReviewCard.js
+++ hb-v2/src/components/Card/ReviewCard.js
@@ -130,12 +130,12 @@ function ReviewCard(props){
                 </div>
                 <div className="review-footer">
                     <div className="row justify-content-between">
-                        <div className="col-6" style={{display:'flex', flexDirection:'row'}}>
+                        <div className="col" style={{display:'flex', flexDirection:'row'}}>
                             <p>Membantu?</p>
                             <Button className="vote-button" onClick={() => vote("upVote")}>{props.review.helpfulUpVote}<ArrowUpward className="icon"/></Button>
                             <Button className="vote-button" onClick={() => vote("downVote")}>{props.review.helpfulDownVote}<ArrowDownward className="icon"/></Button>
                         </div>
-                        <div className="col-6" style={{display:'flex', justifyContent:'flex-end'}}>
+                        <div className="col" style={{display:'flex', justifyContent:'flex-end'}}>
                             <a href="/" style={{color:'black'}}>Laporkan</a>
                             <EmojiFlagsOutlined className="icon"/>
                         </div>
