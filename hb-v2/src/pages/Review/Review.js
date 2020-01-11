ce380b603811af5ebb130545a5086a429e9bb9cc
Danika Widjaja <danika.dew@utexas.edu>
2020-01-11 14:09:03 -0600

    loading state

--- hb-v2/src/pages/Review/Review.js
+++ hb-v2/src/pages/Review/Review.js
@@ -7,9 +7,11 @@ import "../../css/review.css";
 import { getReviews } from "../../redux/actions";
 import Popup from "../../components/Popup/Popup";
 import LoginPopup from "../../components/Popup/LoginPopup";
+import Feedback from "../../components/Feedback/Feedback";
 
 function ThumbRating(props){
     return (<StyledRating
+        name="thumb-rating"
         icon={<ThumbUp/>}
         emptyIcon={<ThumbUpOutlined/>} 
         className="large-rating"
@@ -28,63 +30,74 @@ function Review(props){
     const revieweeId = props.match.params.revieweeId;
     const loggedIn = props.loggedIn
     const getReviews = props.getReviews;
+
     React.useEffect(() =>{
-        getReviews(revieweeId) 
+        getReviews(revieweeId)
     }, [getReviews, revieweeId, loggedIn])
 
 
-    if(!props.professor){
-        return(<div>Loading</div>)
+    if(props.loading){
+        return(<div className="container content page-container"><p>Loading</p></div>)
     }
-    return(
-        <div className="container content page-container">
-            <header className="review-header">
-                <h2 style={{borderBottom:"4px solid #39A3FF", width:'fit-content', fontSize: "calc(100% + 17px)"}}>{props.professor.name}</h2>
-                <p>{props.professor.school}</p>
-                <div style={{display:'flex', marginTop:'10px'}}>
-                    <StyledRating
-                        precision={0.1}
-                        value={props.professor.overallRating} 
-                        readOnly 
-                        icon={<ThumbUp/>}
-                        emptyIcon={<ThumbUpOutlined/>} 
-                    />
-                    <p style={{fontSize:'11px', marginLeft:'10px', marginBottom:'0px'}}>{props.professor.numberOfReviews} Review</p>
+    else if (props.error){
+        return(
+            <div className="container content page-container">
+                <Feedback color={"danger"} message={props.error.message}/>
+            </div>
+        )
+    }
+    else{
+        return(
+            <div className="container content page-container">
+                <header className="review-header">
+                    <h2 style={{borderBottom:"4px solid #39A3FF", width:'fit-content', fontSize: "calc(100% + 17px)"}}>{props.professor.name}</h2>
+                    <p>{props.professor.school}</p>
+                    <div style={{display:'flex', marginTop:'10px'}}>
+                        <StyledRating
+                            precision={0.1}
+                            value={props.professor.overallRating} 
+                            readOnly 
+                            icon={<ThumbUp/>}
+                            emptyIcon={<ThumbUpOutlined/>} 
+                        />
+                        <p style={{fontSize:'11px', marginLeft:'10px', marginBottom:'0px'}}>{props.professor.numberOfReviews} Review</p>
+                    </div>
+                </header>
+                <div style={{display:"flex", alignItems:'center', flexDirection:'column'}}>
+                    <p className="grey-text">{props.professor.numberOfReviews === 0 ? "Be the first to review!" : "Tulis Review Anda"}</p>
+                    {props.loggedIn && 
+                        <StyledRating
+                            id="addReview"
+                            onChange={addReview}
+                            value={rating}
+                            icon={<ThumbUp/>}
+                            emptyIcon={<ThumbUpOutlined/>} 
+                            className="large-rating"
+                        />
+                    }
+                    {!props.loggedIn && 
+                        <Popup
+                            trigger={{
+                                component:ThumbRating,
+                            }}
+                            content={LoginPopup}
+                        />
+                    }
                 </div>
-            </header>
-            <div style={{display:"flex", alignItems:'center', flexDirection:'column'}}>
-                <p className="grey-text">{props.professor.numberOfReviews === 0 ? "Be the first to review!" : "Tulis Review Anda"}</p>
-                {props.loggedIn && 
-                    <StyledRating
-                        id="addReview"
-                        onChange={addReview}
-                        value={rating}
-                        icon={<ThumbUp/>}
-                        emptyIcon={<ThumbUpOutlined/>} 
-                        className="large-rating"
-                    />
-                }
-                {!props.loggedIn && 
-                    <Popup
-                        trigger={{
-                            component:ThumbRating,
-                        }}
-                        content={LoginPopup}
-                    />
-                }
+                <div style={{margin:'1rem -30px'}}>
+                    <ReviewContent/>
+                </div>
             </div>
-            <div style={{margin:'1rem -30px'}}>
-                <ReviewContent/>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 function mapStateToProps(state){
     return{
         professor: state.professor,
         error: state.error,
-        loggedIn: state.loggedIn
+        loggedIn: state.loggedIn,
+        loading: state.loading
     }
 }
 export default connect(mapStateToProps, {getReviews})(Review);
