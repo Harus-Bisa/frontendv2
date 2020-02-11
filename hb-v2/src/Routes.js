fa014b04b1cc4ddc033265e8bd33b40444aa6de0
Danika Widjaja <danika.dew@utexas.edu>
2020-02-10 19:32:28 -0600

    about page init

--- hb-v2/src/Routes.js
+++ hb-v2/src/Routes.js
@@ -7,11 +7,13 @@ import Login from './pages/Login/Login';
 import SignUp from './pages/Sign Up/SignUp';
 import Verification from './pages/Verification/Verification';
 import Query from './pages/Query/Query';
+import About from './pages/About/About';
 
 function Routes() {
     return(
         <Switch>
             <Route exact path="/" component={Landing}/>
+            <Route exact path="/about" component={About}/>
             <Route exact path={["/review/new/:revieweeName", "/review/:revieweeId/add/:overallRating" ]}component={ReviewForm}/>
             <Route exact path="/review/:revieweeId" component={Review}/>
             <Route exact path="/query" component={Query}/>
