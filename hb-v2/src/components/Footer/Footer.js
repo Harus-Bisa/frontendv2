6053a499fdf28c29a42afc8406a46f4df268c47c
Danika Widjaja <danika.dew@utexas.edu>
2020-02-11 23:01:37 -0600

    mobile footer

--- hb-v2/src/components/Footer/Footer.js
+++ hb-v2/src/components/Footer/Footer.js
@@ -8,10 +8,10 @@ export default function Footer(){
     return(
         <div className="footer container">
             <div className="row justify-content-center no-gutters">
-                <div className="col-lg-3 col-md-4 col-6" style={{display:'flex'}}>
-                    <h2 style={{margin:'auto'}}>DOSEN KU</h2>
+                <div className="col-lg-3 col-md-4 col-12" style={{display:'flex'}}>
+                    <h2 style={{margin:'auto'}}>DOSEN <span className="blue">KU</span></h2>
                 </div> 
-                <div className="col-lg-3 col-md-4 col-6">
+                <div className="col-lg-3 col-md-4 col-12">
                     <div className="row justify-content-center">
                         <IconButton>
                             <IoLogoFacebook/>
@@ -25,24 +25,24 @@ export default function Footer(){
                     </div>
                 </div>               
             </div>
-            <div className="row links justify-content-center">
-                <div className="col col-lg-2">
+            <div className="row links no-gutters justify-content-center">
+                <div className="col-3 col-lg-2 flex">
                     <a href="/help">Bantuan</a>
                 </div>
                 <Divider orientation="vertical"/>
-                <div className="col col-lg-2">
+                <div className="col-4 col-lg-2 flex">
                     <a href="/about">Tentang Kami</a>
                 </div>
                 <Divider orientation="vertical"/>
-                <div className="col col-lg-2">
+                <div className="col-4 col-lg-2 flex">
                     <a href="/">Kebijakan Privasi</a>
                 </div>
                 <Divider orientation="vertical"/>
-                <div className="col col-lg-2">
+                <div className="col-5 col-lg-2 flex">
                     <a href="/">Syarat & Kententuan</a>
                 </div>
                 <Divider orientation="vertical"/>
-                <div className="col col-lg-2">
+                <div className="col-5 col-lg-2 flex">
                     <a href="/">Peraturan Konten</a>
                 </div>
             </div>
